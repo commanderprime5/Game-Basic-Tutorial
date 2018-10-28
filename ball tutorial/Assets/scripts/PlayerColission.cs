175eca2cd85995394db569a3613367a784fcbe65
Michiel Cools <michielcools@gmail.com>
2018-10-28 16:58:22 +0100

    Add Game Ep07

---
+++ ball tutorial/Assets/scripts/PlayerColission.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerColission : MonoBehaviour{
+
+    public playerMovement movement;
+    
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstakel")
+        {
+            movement.enabled = false;
+            FindObjectOfType <GameManager>().EndGame();
+        }
+
+    }
+
+}
+
+  
+
+
+	
